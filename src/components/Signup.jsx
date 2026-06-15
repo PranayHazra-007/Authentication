9af9532ad46ef1a6cdb5a9cb80a14c19a9e2ec18
Pranay Hazra <pranayhazra034@gmail.com>
2026-06-15 11:51:52 +0530

    Updated signup page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,70 +1,34 @@
 import React, { useState } from "react";
 
 const Signup = () => {
-  const [user, setUser] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
+  const [user, setUser] = useState({username: "",email: "",password: ""});
 
   const ChangeUser = (e) => {
     const { name, value } = e.target;
-
     setUser((pre) => ({
       ...pre,
       [name]: value,
     }));
   };
-
   const SignupUser = () => {
     localStorage.setItem("user", JSON.stringify(user));
-
     alert("User data saved successfully!");
-
-    setUser({
-      username: "",
-      email: "",
-      password: "",
-    });
+    setUser({ username: "",email: "",password: ""});
   };
 
   return (
     <div style={{ width: "300px", margin: "100px auto" }}>
       <h2>Signup</h2>
 
-      <input
-        type="text"
-        name="username"
-        placeholder="Username"
-        value={user.username}
-        onChange={ChangeUser}
-      />
-
+      <input type="text" name="username" placeholder="Username" value={user.username} onChange={ChangeUser}/>
       <br />
       <br />
-
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={user.email}
-        onChange={ChangeUser}
-      />
-
+      <input type="email" name="email" placeholder="Email" value={user.email} onChange={ChangeUser} />
       <br />
       <br />
-
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={user.password}
-        onChange={ChangeUser}
-      />
-
+      <input type="password"  name="password" placeholder="Password" value={user.password} onChange={ChangeUser} />
       <br />
       <br />
-
       <button onClick={SignupUser}>Signup</button>
     </div>
   );
